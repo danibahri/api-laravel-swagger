ef578f30b04ce11ce216040133116b9b5a2c3b38
danibahri <danibahri45@gmail.com>
2025-03-04 18:48:21 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users',[UserController::class,'get']);
-Route::post('/users',[UserController::class,'post']);
-Route::get('/users/{id}',[UserController::class,'getById']);
+Route::get('/users',[UserController::class,'get'])->name('api.users');
+Route::post('/users',[UserController::class,'post'])->name('api.users.store');
+Route::get('/users/{id}',[UserController::class,'getById'])->name('api.users.id');
